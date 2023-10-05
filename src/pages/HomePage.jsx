b904eb93a76517469c7ffa6a50d8b92fba1821a0
JanJaap <j-jschenk@hotmail.com>
2023-10-05 15:18:40 +0200

    implemented basic router navigation

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <h1>Welcome Resident IronHack</h1>;
+};
+
+export default HomePage;
---
+++ src/pages/Scene.jsx
@@ -0,0 +1,9 @@
+const Scene = () => {
+  return (
+    <>
+      <h1>Welcome to our Scene Page</h1>
+    </>
+  );
+};
+
+export default Scene;
