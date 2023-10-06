2e2f772cc72007e8a6d4d41feb5fb0ca395258b9
JanJaap <j-jschenk@hotmail.com>
2023-10-06 12:42:56 +0200

    implemented simple fetch

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react";
+
 const HomePage = () => {
+  const [scenes, setScenes] = useState([]);
+
+  const fetchAllScenes = async () => {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/scenes`);
+    if (response.ok) {
+      const allScenes = await response.json();
+      setScenes(allScenes);
+      console.log(allScenes);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllScenes();
+  }, []);
+
   return <h1>Welcome Resident IronHack</h1>;
 };
 
