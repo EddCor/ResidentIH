d63891debb489d42e5343c782bef6cb449657730
JanJaap <j-jschenk@hotmail.com>
2023-10-12 12:25:05 +0200

    Updated menu list, made about page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Scene from "./pages/Scene";
 import CreateNewStory from "./pages/CreateNewStory";
 import NavBar from "./components/NavBar";
 import { Route, Routes } from "react-router-dom";
+import About from "./pages/Scene";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/scene/:sceneId" element={<Scene />} />
         <Route path="/createnewstory" element={<CreateNewStory />} />
+        <Route path="/about" element={<About />} />
         <Route path="*" element={<h1>404 Page</h1>} />
       </Routes>
     </>
